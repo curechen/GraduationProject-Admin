1d68d6818ab989b88a5875583f0d43793ecf9afb
curechen <981470148@qq.com>
2023-01-10 21:49:59 +0800

    chore: 修改 >>> 为 :deep()，这是兼容性调整

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 export default {
   development: {
-    // baseUrl: '/api' // 测试接口域名
-    baseUrl: '//42.193.99.39:8091/manage-api/v1' // 测试接口域名
+    baseUrl: '/api' // 测试接口域名
+    // baseUrl: '//42.193.99.39:8091/manage-api/v1' // 测试接口域名
   },
   beta: {
     // baseUrl: '//backend-api-02.newbee.ltd/manage-api/v1' // 测试接口域名
